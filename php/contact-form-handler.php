497cf4d6eee99ca0f2974f411cf46a4ed83bae5d
Brandon Birk <bbirk@sfu.ca>
2019-04-03 20:18:39 -0700

    Validated Files

--- php/contact-form-handler.php
+++ php/contact-form-handler.php
@@ -1,22 +1,18 @@
 <?php
+
+if(isset($_POST['submit'])) {
     $name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$message = $_POST['message'];
+    $mailFrom = $_POST['mail'];
+    $message = $_POST['message'];
+    
+    $mailTo="brandonbirk1995@gmail.com";
+    $headers = "From: ".$mailFrom;
+    $txt = "You have received an e-mail from ".$name.".\n\n".message;
+    
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: index.php?mailsend");
+}
 
-$email_from = 'contact@shotbybirk.com';
 
-$email_subject = "New Email";
 
-$email_body = "User Name: $name.\n".
-                "User Email: $visitor_email.\n";
 
-$to = "brandonbirk1995@gmail.com";
-
-$headers = "From: $email_from \r\n";
-
-$headers .= "Reply-To: $visitor_email \r\n";
-
-mail($to,$email_subject,$email_body,$headers);
-
-header("Location: contact.html")
-?>
